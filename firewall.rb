8f14b4d608934bc29d5ba4c6595256987e3e89e9
Ameya Thakur <redsupercar@gmail.com>
2020-02-07 14:07:56 -0800

    fix newline bug with strip

--- firewall.rb
+++ firewall.rb
@@ -4,7 +4,7 @@ class Firewall
         @rules = Array.new(4) {Array.new(65536)}
 
         File.foreach(path) do |line|
-            rule = line.split(',')
+            rule = line.strip.split(',')
             port = rule[2].split('-')
             ip_address = rule[3].split('-')
 
@@ -16,6 +16,7 @@ class Firewall
                 rules_index = rule[1] === "tcp" ? 2 : 3
             end
 
+            #if range of ports
             if port.length() > 1 
                 for i in port[0].to_i .. port[1].to_i
                     if @rules[rules_index][i].nil?
@@ -29,14 +30,18 @@ class Firewall
                         @rules[rules_index][i][ip_address[0]] = true
                     end
                 end
+            #single port
             else
+                #hash doesn't exist for this port yet
                 if @rules[rules_index][port[0].to_i].nil?
                     @rules[rules_index][port[0].to_i] = Hash.new
                 end
+                #range of IPs
                 if ip_address.length() > 1
                     for j in ip_to_number(ip_address[0]) .. ip_to_number(ip_address[1])
                         @rules[rules_index][port[0].to_i][number_to_ip(j)] = true
                     end
+                #single IP
                 else
                     @rules[rules_index][port[0].to_i][ip_address[0]] = true
                 end
@@ -55,6 +60,7 @@ class Firewall
             rules_index = protocol === "tcp" ? 2 : 3
         end
 
+        #check rule
         if @rules[rules_index][port].nil? || @rules[rules_index][port][ip_address].nil?
             false
         else
@@ -62,11 +68,13 @@ class Firewall
         end
     end
 
+    #convert IP string to number to get range for hash
     def ip_to_number(ip_address)
         chunks = ip_address.split('.').map(&:to_i)
         number = (chunks[0] << 24) + (chunks[1] << 16) + (chunks[2] << 8) + chunks[3]
     end
 
+    #convert number to IP string to use as hash key
     def number_to_ip(number)
         chunks = Array.new(4)
         chunks[3] = (255 & number).to_s
@@ -75,12 +83,16 @@ class Firewall
         chunks[0] = (number >> 24).to_s
         chunks.join(".")
     end
+
+    def get_rules
+        @rules
+    end
 end
 
-
+#testing
 fw = Firewall.new("./fw.csv")
-puts fw.accept_packet("inbound", "udp", 53, "192.168.2.1")
-puts fw.accept_packet("outbound", "tcp", 10234, "192.168.10.11")
-
-puts fw.accept_packet("inbound", "tcp", 81, "192.168.1.2")
-puts fw.accept_packet("inbound", "udp", 24, "52.12.48.92")
+#puts fw.accept_packet("inbound", "udp", 53, "192.168.2.1")
+#puts fw.accept_packet("outbound", "tcp", 10234, "192.168.10.11")
+#p fw.get_rules[2][10234]
+#puts fw.accept_packet("inbound", "tcp", 81, "192.168.1.2")
+#puts fw.accept_packet("inbound", "udp", 24, "52.12.48.92")
